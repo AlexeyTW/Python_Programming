2e8d28a0ff019c15ae7469f779426f538ab7d2d5
Alexey Barinov <alexey.barinov@transwareag.com>
2021-10-20 18:23:55 +0200

    Classes are done, function is pending

--- playground/solution.py
+++ playground/solution.py
@@ -3,8 +3,8 @@ import csv
 
 class CarBase:
 	def __init__(self, brand, photo_file_name, carrying):
-		self.brand = brand
-		self.photo_file_name = photo_file_name
+		self.brand = str(brand)
+		self.photo_file_name = str(photo_file_name)
 		self.carrying = float(carrying)
 
 	def get_photo_file_ext(self):
@@ -13,45 +13,69 @@ class CarBase:
 
 class Car(CarBase):
 	car_type = 'car'
-	def __init__(self, brand, photo_file_name, carrying, passenger_seats_count=None):
+	def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
 		super(Car, self).__init__(brand, photo_file_name, carrying)
-		self.passenger_seats_count = passenger_seats_count
+		self.passenger_seats_count = int(passenger_seats_count)
 
 
 class Truck(CarBase):
 	car_type = 'truck'
 	def __init__(self, brand, photo_file_name, carrying, body_lwh):
 		super(Truck, self).__init__(brand, photo_file_name, carrying)
-		self.body_length = body_lwh.split('x')[0]
-		self.body_width = body_lwh.split('x')[1]
-		self.body_height = body_lwh.split('x')[2]
+		self.body_lwh = body_lwh
 
-	def get_body_volume(self):
-		self.body_params = (self.body_length, self.body_width, self.body_height)
+	@property
+	def body_lwh(self):
+		return self._body_lwh
+
+	@body_lwh.setter
+	def body_lwh(self, value):
 		try:
-			self.body_params = list(map(float, self.body_params))
-			volume = self.body_params[0] * self.body_params[1] * self.body_params[2]
-			return volume
+			self.body_params = list(map(float, value.split('x')))
+			self.body_length = self.body_params[0]
+			self.body_width = self.body_params[1]
+			self.body_height = self.body_params[2]
 		except ValueError:
 			self.body_width, self.body_height, self.body_length = 0, 0, 0
-			return self.body_width, self.body_height, self.body_length
+
+
+	def get_body_volume(self):
+		return self.body_length * self.body_width * self.body_height
 
 
 class SpecMachine(CarBase):
 	car_type = 'spec_machine'
 	def __init__(self, brand, photo_file_name, carrying, extra):
 		super(SpecMachine, self).__init__(brand, photo_file_name, carrying)
-		self.extra = extra
+		self.extra = str(extra)
 
 
 def get_car_list(filename):
+	cars = []
 	with open(filename, 'r', encoding='utf-8') as file:
 		reader = csv.reader(file, delimiter=';')
-		col_indexes = enumerate(['car_type', 'brand', 'passenger_seats_count', 'photo_file_name', 'body_whl', 'carrying', 'extra'])
-		print(col_indexes)
-		#next(reader)
+		next(reader)
 		for row in reader:
-			print(row)
+			if len(row) < 7:
+				continue
+			car_type, brand, passenger_seats_count, photo_file_name, body_lwh, carrying, extra = row
+			if car_type == 'car':
+				try:
+					obj = Car(brand, photo_file_name, carrying, passenger_seats_count)
+					cars.append(obj)
+				except ValueError:
+					continue
+			elif car_type == 'truck':
+				try:
+					obj = Truck(brand, photo_file_name, carrying, body_lwh)
+					cars.append(obj)
+				except Exception as ex:
+					print(ex.args[0])
+		return cars
 
-get_car_list('coursera_week3_cars.csv')
+#cars = get_car_list('coursera_week3_cars.csv')
+#print(cars[1].__dict__)
+truck = Truck('Nissan', 'nissan.jpeg', '1.5', '2x8x1.87')
+print(truck.__dict__)
+print(truck.get_body_volume())
 
