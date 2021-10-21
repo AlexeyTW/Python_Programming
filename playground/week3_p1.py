9d5fbc0a548844e4d72c91670f559f23f1140202
Alexey Barinov <alexey.barinov@transwareag.com>
2021-10-21 15:22:07 +0200

    Classes are done, programming task is done

--- playground/week3_p1.py
+++ playground/week3_p1.py
@@ -1,4 +1,4 @@
-from solution import FileReader
+from week3_p2 import FileReader
 
 reader = FileReader('some_file1.txt')
 text = reader.read()
--- playground/week3_p2.py
+++ playground/week3_p2.py
@@ -8,6 +8,10 @@ class CarBase:
 		self.carrying = float(carrying)
 
 	def get_photo_file_ext(self):
+		if self.photo_file_name.find('.') in [-1, 0, len(self.photo_file_name) - 1]:
+			raise ValueError('Incorrect file extension')
+		if self.photo_file_name.count('.') != 1:
+			raise ValueError('Incorrect file extension')
 		return '.' + self.photo_file_name.split('.')[1]
 
 
@@ -26,17 +30,18 @@ class Truck(CarBase):
 
 	@property
 	def body_lwh(self):
-		return self._body_lwh
+		return self.body_lwh
 
 	@body_lwh.setter
 	def body_lwh(self, value):
 		try:
 			self.body_params = list(map(float, value.split('x')))
+			assert len(self.body_params) == 3
 			self.body_length = self.body_params[0]
 			self.body_width = self.body_params[1]
 			self.body_height = self.body_params[2]
-		except ValueError:
-			self.body_width, self.body_height, self.body_length = 0, 0, 0
+		except Exception:
+			self.body_width, self.body_height, self.body_length = list(map(float, [0, 0, 0]))
 
 
 	def get_body_volume(self):
@@ -59,23 +64,37 @@ def get_car_list(filename):
 			if len(row) < 7:
 				continue
 			car_type, brand, passenger_seats_count, photo_file_name, body_lwh, carrying, extra = row
-			if car_type == 'car':
+			if not (bool(brand) and bool(photo_file_name) and bool(carrying) and bool(car_type)):
+				continue
+			if car_type == 'car' and bool(passenger_seats_count):
 				try:
 					obj = Car(brand, photo_file_name, carrying, passenger_seats_count)
+					obj.get_photo_file_ext()
 					cars.append(obj)
 				except ValueError:
 					continue
 			elif car_type == 'truck':
 				try:
 					obj = Truck(brand, photo_file_name, carrying, body_lwh)
+					obj.get_photo_file_ext()
 					cars.append(obj)
-				except Exception as ex:
-					print(ex.args[0])
+				except Exception:
+					continue
+			elif car_type == 'spec_machine' and bool(extra):
+				try:
+					obj = SpecMachine(brand, photo_file_name, carrying, extra)
+					obj.get_photo_file_ext()
+					cars.append(obj)
+				except Exception:
+					continue
 		return cars
 
-#cars = get_car_list('coursera_week3_cars.csv')
-#print(cars[1].__dict__)
-truck = Truck('Nissan', 'nissan.jpeg', '1.5', '2x8x1.87')
-print(truck.__dict__)
-print(truck.get_body_volume())
+cars = get_car_list('coursera_week3_cars.csv')
+print(len(cars))
+print(cars[-1].__dict__)
+print(cars[-1].get_photo_file_ext())
+#truck = Truck('nissan', 'nissan.jpg', '1.5', '3x4x5x6')
+#print(truck.__dict__)
+#print(type(truck.body_length))
+#print(truck.get_body_volume())
 
