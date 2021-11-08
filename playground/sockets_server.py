8692c02e159ce2c35a2337d19ae8336024c48240
Alexey Barinov <barinov_a@inbox.ru>
2021-11-09 00:30:06 +0300

    Sockets are in progress

---
+++ playground/sockets_server.py
@@ -0,0 +1,13 @@
+import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind(('127.0.0.1', 10001))
+    sock.listen(socket.SOMAXCONN)
+    while True:
+        conn, addr = sock.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(data.decode('utf-8'))
