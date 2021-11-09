ffe3ecea6ad20d29ca1195c29abbe00532d5d5c1
Alexey Barinov <alexey.barinov@transwareag.com>
2021-11-09 16:05:35 +0100

    Sockets are in progress

--- playground/sockets_client.py
+++ playground/sockets_client.py
@@ -1,4 +1,15 @@
 import socket
 
-with socket.create_connection(('127.0.0.1', 10001)) as sock:
-    sock.sendall('hello'.encode('utf-8'))
+'''with socket.create_connection(('192.168.123.143', 10001)) as sock:
+    sock.sendall('hello'.encode('utf-8'))'''
+
+
+# ===================================
+with socket.create_connection(('192.168.123.143', 10001), 5) as sock:
+    sock.settimeout(2)
+    try:
+        sock.sendall(b'timeout')
+    except socket.timeout:
+        print('send data timeout')
+    except socket.error as ex:
+        print('socket error: ', ex)
--- playground/sockets_server.py
+++ playground/sockets_server.py
@@ -1,7 +1,7 @@
 import socket
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind(('127.0.0.1', 10001))
+# =================================
+'''with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind(('0.0.0.0', 10001))
     sock.listen(socket.SOMAXCONN)
     while True:
         conn, addr = sock.accept()
@@ -10,4 +10,61 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 data = conn.recv(1024)
                 if not data:
                     break
+                print(data.decode('utf8'))'''
+
+# ======================================
+'''sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('192.168.123.143', 10001))
+sock.listen(socket.SOMAXCONN)
+
+conn, addr = sock.accept()
+conn.settimeout(5)
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+
+conn.close()
+sock.close()
+'''
+# Timeouts ==============================
+
+with socket.socket() as sock:
+    sock.bind(("", 10001))
+    sock.listen()
+    while True:
+        conn, addr = sock.accept()
+        conn.settimeout(10)
+        with conn:
+            while True:
+                try:
+                    data = sock.recv(1024)
+                except socket.timeout:
+                    print('Connection is closed by timeout')
+                    break
+
+                if not data:
+                    break
                 print(data.decode('utf-8'))
+
+'''sock = socket.socket()
+sock.bind(('192.168.123.143', 10001))
+sock.listen()
+
+conn, addr = sock.accept()
+conn.settimeout(30)
+
+while True:
+    try:
+        data = sock.recv(1024)
+    except socket.timeout:
+        print('Socket timeout error')
+        break
+
+    if not data:
+        break
+
+    print(data.decode('utf-8'))
+conn.close()
+sock.close()'''
