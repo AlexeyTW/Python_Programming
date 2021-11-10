f072118d27872f413dc0caadd475935bcbb95d5f
Alexey Barinov <barinov_a@inbox.ru>
2021-11-11 00:29:19 +0300

    Generators are in progress

--- playground/generators.py
+++ playground/generators.py
@@ -4,6 +4,37 @@ def MyRangeGenerator(top):
         yield current
         current += 1
 
-counter = MyRangeGenerator(3)
-for i in counter:
+# ==============   COROUTINES   ==================
+
+'''def grep(pattern):
+    print('start grep')
+    try:
+        while True:
+            line = yield
+            if pattern in line:
+                print(line)
+    except GeneratorExit:
+        print('coroutine is closed')'''
+
+def grep(pattern):
+    print('grep started')
+    while True:
+        line = yield
+        if pattern in line:
+            print(line)
+
+
+def grep_coroutine():
+    g = grep('ttt')
+    yield from g
+
+
+def chain(x_iter, y_iter):
+    yield from x_iter
+    yield from y_iter
+
+a = [1, 2, 3]
+b = (4, 5)
+
+for i in chain(a, b):
     print(i)
