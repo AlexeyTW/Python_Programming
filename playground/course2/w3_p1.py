63e9095b418eee5bee53544571f4c227c7cb7988
Alexey Barinov <barinov_a@inbox.ru>
2021-11-30 00:50:47 +0300

    Class decorators are in progress

--- playground/course2/w3_p1.py
+++ playground/course2/w3_p1.py
@@ -1,69 +1,90 @@
 from abc import ABC, abstractmethod
 
 
-class Hero():
-	def __init__(self):
-		self.positive_effects = []
-		self.negative_effects = []
-		self.common_stats = ['Strength', 'Perception', 'Endurance', 'Charisma',
-							 'Intelligence', 'Agility', 'Luck'
-							 ]
-		self.stats = {
-			"HP": 128,  # health points
-			"MP": 42,  # magic points,
-			"SP": 100,  # skill points
-			"Strength": 15,  # сила
-			"Perception": 4,  # восприятие
-			"Endurance": 8,  # выносливость
-			"Charisma": 2,  # харизма
-			"Intelligence": 3,  # интеллект
-			"Agility": 8,  # ловкость
-			"Luck": 1  # удача
-		}
+class Hero:
+    def __init__(self):
+        self.positive_effects = []
+        self.negative_effects = []
+        self.common_stats = ['Strength', 'Perception', 'Endurance', 'Charisma',
+                             'Intelligence', 'Agility', 'Luck']
+        self.stats = {
+            "HP": 128,  # health points
+            "MP": 42,  # magic points,
+            "SP": 100,  # skill points
+            "Strength": 15,  # сила
+            "Perception": 4,  # восприятие
+            "Endurance": 8,  # выносливость
+            "Charisma": 2,  # харизма
+            "Intelligence": 3,  # интеллект
+            "Agility": 8,  # ловкость
+            "Luck": 1  # удача
+        }
 
-	def get_positive_effects(self):
-		return self.positive_effects.copy()
+    def get_positive_effects(self):
+        # return self.positive_effects.copy()
+        print(self.positive_effects.copy())
 
-	def get_negative_effects(self):
-		return self.negative_effects.copy()
+    def get_negative_effects(self):
+        return self.negative_effects.copy()
 
-	def get_stats(self):
-		return self.stats.copy()
+    def get_stats(self):
+        # return self.stats.copy()
+        print(self.stats.copy())
+
 
 class AbstractEffect(Hero):
-	pass
+    pass
 
 
 class AbstractPositive(AbstractEffect):
-	pass
+    pass
 
 
 class AbstractNegative(AbstractEffect):
-	pass
+    pass
 
 
 class Berserk(AbstractPositive):
-	def apply_berserk(self, obj):
-		self.obj = obj
-		obj.stats['HP'] += 7
+    def __init__(self, obj):
+        super().__init__()
+        self.obj = obj
+
+        for key in ['Strength', 'Endurance', 'Agility', 'Luck']:
+            self.obj.stats[key] += 7
+        for key in ['Perception', 'Charisma', 'Intelligence']:
+            self.obj.stats[key] -= 3
+        self.obj.stats['HP'] += 50
+        obj.positive_effects.append(self.__class__.__name__)
 
 
 class Blessing(AbstractPositive):
-	pass
+    def __init__(self, obj):
+        super().__init__()
+        self.obj = obj
+
+        for key in self.common_stats:
+            self.stats[key] += 2
 
 
 class Weakness(AbstractNegative):
-	pass
+    pass
 
 
 class Curse(AbstractNegative):
-	pass
+    pass
 
 
 class EvilEye(AbstractNegative):
-	pass
+    pass
 
 
 hero = Hero()
-bers = Berserk(hero)
-print(bers.get_stats())
+print(hero.stats)
+
+brs1 = Berserk(hero)
+brs1.get_stats()
+#hero.get_stats()
+
+brs2 = Berserk(brs1)
+brs2.get_stats()
+#hero.get_stats()
