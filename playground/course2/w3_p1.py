a78c471e69417bf1c288c0d5bba902dc9c895aab
Alexey Barinov <alexey.barinov@transwareag.com>
2021-11-30 15:48:12 +0100

    Clas decorators are in progress

--- playground/course2/w3_p1.py
+++ playground/course2/w3_p1.py
@@ -1,8 +1,24 @@
 from abc import ABC, abstractmethod
 
 
-class Hero:
-    def __init__(self):
+class Hero(ABC):
+    positive_effects = []
+    negative_effects = []
+    stats = {
+        "HP": 128,  # health points
+        "MP": 42,  # magic points,
+        "SP": 100,  # skill points
+        "Strength": 15,  # сила
+        "Perception": 4,  # восприятие
+        "Endurance": 8,  # выносливость
+        "Charisma": 2,  # харизма
+        "Intelligence": 3,  # интеллект
+        "Agility": 8,  # ловкость
+        "Luck": 1  # удача
+    }
+    common_stats = ['Strength', 'Perception', 'Endurance', 'Charisma',
+                    'Intelligence', 'Agility', 'Luck']
+    '''def __init__(self):
         self.positive_effects = []
         self.negative_effects = []
         self.common_stats = ['Strength', 'Perception', 'Endurance', 'Charisma',
@@ -18,73 +34,105 @@ class Hero:
             "Intelligence": 3,  # интеллект
             "Agility": 8,  # ловкость
             "Luck": 1  # удача
-        }
+        }'''
+
 
     def get_positive_effects(self):
         # return self.positive_effects.copy()
         print(self.positive_effects.copy())
 
+
     def get_negative_effects(self):
         return self.negative_effects.copy()
 
+
     def get_stats(self):
-        # return self.stats.copy()
-        print(self.stats.copy())
+        return self.stats.copy()
 
 
 class AbstractEffect(Hero):
-    pass
+    def __init__(self, obj):
+        self.obj = obj
+
+    def get_stats(self):
+        return self.obj.stats.copy()
 
 
 class AbstractPositive(AbstractEffect):
-    pass
+    '''def __init__(self, obj):
+        super().__init__(obj)
+        self.obj = obj
+        self.obj.positive_effects.append(self.__class__.__name__)'''
+
+    def get_positive_effects(self):
+        return self.obj.positive_effects.copy()
 
 
 class AbstractNegative(AbstractEffect):
-    pass
+    '''def __init__(self, obj):
+        super().__init__(obj)
+        self.obj = obj
+        self.obj.negative_effects.append(self.__class__.__name__)'''
+
+    def get_negative_effects(self):
+        return self.negative_effects.copy()
 
 
 class Berserk(AbstractPositive):
     def __init__(self, obj):
-        super().__init__()
-        self.obj = obj
+        super().__init__(obj)
 
         for key in ['Strength', 'Endurance', 'Agility', 'Luck']:
             self.obj.stats[key] += 7
         for key in ['Perception', 'Charisma', 'Intelligence']:
             self.obj.stats[key] -= 3
         self.obj.stats['HP'] += 50
-        obj.positive_effects.append(self.__class__.__name__)
 
 
 class Blessing(AbstractPositive):
     def __init__(self, obj):
-        super().__init__()
-        self.obj = obj
+        super().__init__(obj)
 
-        for key in self.common_stats:
+        for key in self.obj.common_stats:
             self.stats[key] += 2
 
 
 class Weakness(AbstractNegative):
-    pass
+    def __init__(self, obj):
+        super().__init__(obj)
+
+        for key in ['Strength', 'Endurance', 'Agility']:
+            self.obj.stats[key] -= 4
 
 
 class Curse(AbstractNegative):
-    pass
+    def __init__(self, obj):
+        super().__init__(obj)
+
+        for key in self.obj.common_stats:
+            self.obj.stats[key] -= 2
 
 
 class EvilEye(AbstractNegative):
-    pass
+    def __init__(self, obj):
+        super().__init__(obj)
+
+        self.obj.stats['Luck'] -= 10
+
 
 
 hero = Hero()
-print(hero.stats)
+print(hero.get_stats())
+print(hero.positive_effects, hero.negative_effects)
 
-brs1 = Berserk(hero)
-brs1.get_stats()
-#hero.get_stats()
+bers1 = Berserk(hero)
+print(bers1.get_stats())
+print(bers1.positive_effects, bers1.negative_effects)
 
-brs2 = Berserk(brs1)
-brs2.get_stats()
-#hero.get_stats()
+curse1 = Curse(bers1)
+print(curse1.get_stats())
+print(curse1.positive_effects, curse1.negative_effects)
+
+ee = EvilEye(bers1)
+print(ee.stats)
+print(ee.positive_effects, ee.negative_effects)
