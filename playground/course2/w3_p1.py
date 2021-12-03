3d7c98bf690be9f64187f9266d222716650342a4
Alexey Barinov <alexey.barinov@transwareag.com>
2021-12-03 14:06:39 +0100

    Class decorators are in progress

--- playground/course2/w3_p1.py
+++ playground/course2/w3_p1.py
@@ -33,8 +33,8 @@ class Hero:
 class AbstractEffect(ABC, Hero):
     def __init__(self, obj):
         self.base = obj
-        self.common_stats = ['Strength', 'Perception', 'Endurance', 'Charisma',
-                             'Intelligence', 'Agility', 'Luck']
+        self.positive_effects = obj.positive_effects
+        self.negative_effects = obj.negative_effects
 
     @abstractmethod
     def get_stats(self):
@@ -49,26 +49,34 @@ class AbstractEffect(ABC, Hero):
         pass
 
 
-'''class AbstractPositive(AbstractEffect, ABC):
+class AbstractPositive(AbstractEffect):
     def __init__(self, obj):
-        self.positive_effects.append(obj.__class__.__name__)
+        super().__init__(obj)
+        self.base = obj
+        self.positive_effects.append(self.__class__.__name__)
 
+    def get_positive_effects(self):
+        self.base.get_positive_effects()
+
+    def get_negative_effects(self):
+        self.base.get_negative_effects()
 
 
 class AbstractNegative(AbstractEffect):
-    def get_stats(self):
-        self.get_stats()
+    def __init__(self, obj):
+        super().__init__(obj)
+        self.base = obj
+        self.negative_effects.append(self.__class__.__name__)
 
     def get_positive_effects(self):
-        self.get_positive_effects()
+        return self.positive_effects
 
     def get_negative_effects(self):
-        self.get_negative_effects()'''
+        return self.negative_effects
 
 
-class Berserk(AbstractEffect):
+class Berserk(AbstractPositive):
     def get_stats(self):
-        self.stats = dict(self.base.stats)
         for key in ['Strength', 'Endurance', 'Agility', 'Luck']:
             self.stats[key] += 7
         for key in ['Perception', 'Charisma', 'Intelligence']:
@@ -77,19 +85,26 @@ class Berserk(AbstractEffect):
         return self.stats
 
     def get_positive_effects(self):
-        self.positive_effects = self.base.get_positive_effects()
-        self.positive_effects.append(self.__class__.__name__)
         return self.positive_effects
 
     def get_negative_effects(self):
-        self.negative_effects = self.base.get_negative_effects()
-        self.negative_effects.append(self.__class__.__name__)
         return self.negative_effects
 
+class Curse(AbstractNegative):
+    def get_stats(self):
+        for key in self.common_stats:
+            self.stats[key] -= 2
+        return self.stats
 
 hero = Hero()
 print('Hero: ', hero.get_stats(), hero.get_positive_effects(), hero.get_negative_effects())
 
 bers1 = Berserk(hero)
 print('Bers1: ', bers1.get_stats(), bers1.get_positive_effects(), bers1.get_negative_effects())
+
+brs2 = Berserk(bers1)
+print('Brs 2: ', brs2.stats, brs2.get_positive_effects(), brs2.get_negative_effects())
+
+cur1 = Curse(brs2)
+print(cur1.get_stats())
 
