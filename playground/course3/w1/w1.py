ac264ffeeaefedc8ba37f12b04b277783cd22d75
Alexey Barinov <barinov_a@inbox.ru>
2021-12-27 00:38:12 +0300

    W1 programming task has been started

--- playground/course3/w1/w1.py
+++ playground/course3/w1/w1.py
@@ -3,5 +3,7 @@ import re
 text = 'loremc-=a+10ipsuma-=adb+=10olorsitamet.'
 
 print(re.findall(r'[abc][+|-]=[abc]|\d+', text))
+
+
 #print(re.sub(r'(\w)\1', lambda x: x.group(0).upper(), text))
 #print(re.sub(r"\b(\w*(\w)\2\w*\b)", r'[\1]', text))
