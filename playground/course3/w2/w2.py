427128c013f65f3f8bb868572976ec9e72af290d
Alexey Barinov <barinov_a@inbox.ru>
2021-12-29 00:16:47 +0300

    W2 has been started

--- playground/course3/w2/w2.py
+++ playground/course3/w2/w2.py
@@ -1,3 +1,10 @@
+from bs4 import BeautifulSoup
 import requests
-import re
+url = 'http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm'
+html = requests.get(url)
 
+soup = BeautifulSoup(html.text, 'lxml')
+tags = soup('a', 'extiw')
+#print([tag['href'] for tag in tags])
+#print([tag.name for tag in soup.script.parents])
+print(list(soup.script.children))
