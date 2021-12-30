c3de660405394a4393f8687542548db352d1f37f
Alexey Barinov <barinov_a@inbox.ru>
2021-12-31 01:19:33 +0300

    W2 programming is in progress

--- playground/course3/w2/w2_pa.py
+++ playground/course3/w2/w2_pa.py
@@ -5,27 +5,29 @@ def parse(path_to_file):
 	with open(path_to_file, 'r', encoding='utf-8') as file:
 		data = BeautifulSoup(file.read(), 'lxml')
 		soup = data.find(id="bodyContent")
+		print(set([i.name for i in soup.find_all()]))
 
 		imgs_source = soup.find_all(name='img')
-		img_count = len([img for img in imgs_source if int(img['width']) >= 200]) # RETURN
+		imgs = len([img for img in imgs_source if img.has_attr('width') and int(img['width']) >= 200])
 
 		headers_source = [h.text for h in soup.find_all(re.compile('h\d'))]
-		headers_count = len([h for h in headers_source if str(h).startswith(('E', 'T', 'C'))]) # RETURN
+		headers = len([h for h in headers_source if str(h).startswith(('E', 'T', 'C'))])
 
 		a_source = soup.find_all('a')
 		a_list = []
 		for i in a_source:
 			a_list.append(count_siblings(i))
-		a_max_len = sorted(a_list)[-1]
+		linkslen = sorted(a_list)[-1]
 
-		lists_source = [l for l in soup.find_all('ul')]
-		lst = lists_source[6]
+		lists_source = [l for l in soup.find_all('ul')] + [l for l in soup.find_all('ol')]
+		print(len(lists_source))
+		lists = count_parent_lists(lists_source)
 
 		#print(a.find_next_sibling())
-		#print(soup)
-		print(lst)
-		print(lst.find_parent().name)
-	#print(img_count, headers_count, a_max_len)
+
+		#print(lst)
+		#print(lst.find_parent().name)
+	print(imgs, headers, linkslen, lists)
 
 
 def count_siblings(a, c=1):
@@ -36,11 +38,12 @@ def count_siblings(a, c=1):
 		return count_siblings(a_next, count)
 	return count
 
-def count_parent_lists(lst):
+def count_parent_lists(lists):
 	count = 0
-	if lst.find_parent().name not in ['ul', 'ol']:
-		count += 1
+	for lst in lists:
+		if lst.find_parent().name not in ['li', 'ul', 'ol']:
+			count += 1
 	return count
 
 
-parse('wiki/Stone_Age')
+parse('wiki/Python_(programming_language)')
