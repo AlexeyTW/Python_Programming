70cc91dea52c2dd1461b5fa70594225c27565b7b
Alexey Barinov <barinov_a@inbox.ru>
2022-02-03 00:32:24 +0300

    W4 programming task is in progress

--- playground/course3/w4/coursera_assignment/routing/urls.py
+++ playground/course3/w4/coursera_assignment/routing/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from .views import simple_route
+from django.urls import path, re_path
+from .views import simple_route, slug_route, sum_route
 
 urlpatterns = [
-	path(r'simple_route/', simple_route)
+	path(r'simple_route/', simple_route),
+	re_path(r'(?P<key>slug_route/)(?P<val>[\w-]{1,16}/)', slug_route),
+	re_path(r'sum_route/(-?\d)\/(-?\d)\/', sum_route)
 ]
--- playground/course3/w4/coursera_assignment/routing/views.py
+++ playground/course3/w4/coursera_assignment/routing/views.py
@@ -6,9 +6,16 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def simple_route(request: HttpRequest):
-	#print(request.method, type(request.method))
-	#if HttpResponse.status_code == 200:
-		#return HttpResponse()
-	if request.method == 'GET':
-		print('Not get')
+	if request.method == 'GET' and HttpResponse.status_code == 200:
+		return HttpResponse()
+	if request.method != 'GET':
 		return HttpResponse(405)
+
+@csrf_exempt
+def slug_route(*args, **kwargs):
+	return HttpResponse(kwargs['val'][:-1])
+
+@csrf_exempt
+def sum_route(*args, **kwargs):
+	print(args[-2], args[-1])
+	return HttpResponse(sum(list(map(int, [args[-2], args[-1]]))))
