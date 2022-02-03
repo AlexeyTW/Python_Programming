ee5bd79d6d8c3fbee036687193bdbe706df12b74
Alexey Barinov <alexey.barinov@transwareag.com>
2022-02-03 14:27:53 +0100

    W4 programming task is in progress

--- playground/course3/w4/coursera_assignment/routing/urls.py
+++ playground/course3/w4/coursera_assignment/routing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
-from .views import simple_route, slug_route, sum_route
+from .views import simple_route, slug_route, sum_route, sum_get_method
 
 urlpatterns = [
 	path(r'simple_route/', simple_route),
 	re_path(r'(?P<key>slug_route/)(?P<val>[\w-]{1,16}/)', slug_route),
-	re_path(r'sum_route/(-?\d)\/(-?\d)\/', sum_route)
+	re_path(r'sum_route/(-?\d)\/(-?\d)\/', sum_route),
+	re_path(r'sum_get_method/', sum_get_method)
 ]
--- playground/course3/w4/coursera_assignment/routing/views.py
+++ playground/course3/w4/coursera_assignment/routing/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, HttpResponseBadRequest
 import requests
 from requests import Request
 from django.utils.decorators import method_decorator
@@ -9,6 +9,7 @@ def simple_route(request: HttpRequest):
 	if request.method == 'GET' and HttpResponse.status_code == 200:
 		return HttpResponse()
 	if request.method != 'GET':
+		print(405)
 		return HttpResponse(405)
 
 @csrf_exempt
@@ -17,5 +18,16 @@ def slug_route(*args, **kwargs):
 
 @csrf_exempt
 def sum_route(*args, **kwargs):
-	print(args[-2], args[-1])
 	return HttpResponse(sum(list(map(int, [args[-2], args[-1]]))))
+
+@csrf_exempt
+def sum_get_method(request: HttpRequest):
+	print(request)
+	if request.method == 'GET' and request.content_params is not {}:
+		try:
+			a = int(request.GET['a'])
+			b = int(request.GET['b'])
+		except ValueError:
+			return HttpResponseBadRequest()
+		return HttpResponse(a + b)
+	return None
