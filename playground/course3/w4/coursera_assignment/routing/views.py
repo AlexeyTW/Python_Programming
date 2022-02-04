2f044721327ae268bd7c8790fd926ead46e3c28c
Alexey Barinov <alexey.barinov@transwareag.com>
2022-02-04 16:00:36 +0100

    W4 programming task is in progress

--- playground/course3/w4/coursera_assignment/routing/views.py
+++ playground/course3/w4/coursera_assignment/routing/views.py
@@ -1,8 +1,5 @@
 from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, HttpResponseBadRequest
-from django.http import HttpResponseNotAllowed
-import requests
-from requests import Request
-from django.utils.decorators import method_decorator
+from django.http.response import HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
@@ -10,35 +7,36 @@ def simple_route(request: HttpRequest):
 	if request.method == 'GET' and HttpResponse.status_code == 200:
 		return HttpResponse()
 	if request.method != 'GET':
-		return HttpResponse(405)
+		return HttpResponseNotAllowed('GET')
 
 @csrf_exempt
 def slug_route(*args, **kwargs):
 	return HttpResponse(kwargs['val'][:-1])
 
 @csrf_exempt
-def sum_route(*args, **kwargs):
+def sum_route(request, *args, **kwargs):
+	print(args, kwargs)
 	return HttpResponse(sum(list(map(int, [args[-2], args[-1]]))))
 
 @csrf_exempt
 def sum_get_method(request: HttpRequest):
-	if request.method == 'GET' and request.GET['a'] and request.GET['b']:
+	if request.method == 'GET' and len(request.GET) != 0:
 		try:
 			a = int(request.GET['a'])
 			b = int(request.GET['b'])
 		except ValueError:
 			return HttpResponseBadRequest()
 		return HttpResponse(a + b)
-	return HttpResponseNotFound
+	return HttpResponseNotAllowed('GET')
 
 @csrf_exempt
 def sum_post_method(request: HttpRequest):
-	print(request)
-	if request.method == 'POST' and request.GET['a'] and request.GET['b']:
+	print(request.content_params)
+	if request.method == 'POST' and len(request.GET) != 0:
 		try:
-			a = int(request.GET.get('a'))
-			b = int(request.GET.get('b'))
+			a = int(request.GET['a'])
+			b = int(request.GET['b'])
 		except ValueError:
 			return HttpResponseBadRequest()
 		return HttpResponse(a + b)
-	return HttpResponseNotFound
+	return HttpResponseNotAllowed('POST')
