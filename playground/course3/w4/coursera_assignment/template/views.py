aa79dcfcdf50b4d07069f5b7a586e566688d691d
Alexey Barinov <alexey.barinov@transwareag.com>
2022-02-08 16:03:04 +0100

    W4 programming task 2 is in progress

--- playground/course3/w4/coursera_assignment/template/views.py
+++ playground/course3/w4/coursera_assignment/template/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+from django.template.response import TemplateResponse
 
 
 # Create your views here.
+def get_param(request: HttpRequest):
+    if request.method == 'GET' and request.GET != {}:
+        return list(request.GET.keys())[0]
+    if request.method == 'POST' and request.POST != {}:
+        return list(request.POST.keys())[0]
 
-def echo(request):
-    pass
+
+@csrf_exempt
+def echo(request: HttpRequest):
+    return TemplateResponse(request, 'echo.html', context={
+        'request_type': request.method.lower(),
+        'param': get_param(request),
+        'val': '___'
+    })
 
 
 def filters(request):
