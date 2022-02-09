4eab211db4d388e00a41bae83ca9ec95e3bdb325
Alexey Barinov <barinov_a@inbox.ru>
2022-02-10 00:50:20 +0300

    Django filters are in progress

--- playground/course3/w4/coursera_assignment/template/templatetags/extras.py
+++ playground/course3/w4/coursera_assignment/template/templatetags/extras.py
@@ -1,1 +1,17 @@
 from django import template
+
+register = template.Library()
+
+
+@register.filter(name='inc')
+def inc(n, diff):
+    return int(n) + int(diff)
+
+
+@register.filter(name='division')
+def division(divd, divsr, to_int=False):
+    divd = float(divd)
+    divsr = float(divsr)
+    if to_int:
+        return int(divd / divsr)
+    return divd / divsr
