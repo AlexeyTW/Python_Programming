367743333a93724835875d0dc86440fcc6795430
Alexey Barinov <barinov_a@inbox.ru>
2022-03-10 00:44:24 +0300

    W5 has been started

--- playground/course3/w5/forms/form_dummy/urls.py
+++ playground/course3/w5/forms/form_dummy/urls.py
@@ -1,7 +1,7 @@
 
-from django.urls import path
+from django.conf.urls import url
 from .views import FormDummyView
 
 urlpatterns = [
-    path('form/', FormDummyView.as_view())
+    url('', FormDummyView.as_view())
 ]
