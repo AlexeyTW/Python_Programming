e0fd2d6e80a5e5d97b21a5b3e5e3150c247c85a8
Alexey Barinov <barinov_a@inbox.ru>
2022-04-08 00:36:42 +0300

    W5 Auth is in progress!

--- playground/course3/w5/somemart_auth/somemart/views.py
+++ playground/course3/w5/somemart_auth/somemart/views.py
@@ -1,3 +1,5 @@
+import base64
+
 from django.http import HttpResponse, JsonResponse, HttpRequest
 from django.views import View
 import json
@@ -6,6 +8,7 @@ from jsonschema.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.contrib.auth import authenticate
 
 from .models import Item, Review
 
@@ -38,20 +41,56 @@ class AddItemView(View):
     """View для создания товара."""
 
     def post(self, request: HttpRequest):
+
+        auth64 = request.META.get('HTTP_AUTHORIZATION')
+        auth = base64.b64decode(auth64).decode('ascii')
+        print(auth)
+        username, password = auth.split(':')
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return JsonResponse({}, status=401)
+        if not user.is_staff:
+            return JsonResponse({}, status=403)
         try:
-            print(request.META.get('HTTP_AUTHORIZATION'))
-            return JsonResponse({}, status=201)
-        except (ValidationError, json.JSONDecodeError, TypeError, ValueError):
+            data = json.loads(request.body)
+            if 'price' in data.keys():
+                data['price'] = int(data['price'])
+            validate(data, ITEM_SCHEMA)
+            item = Item(title=data['title'], description=data['description'], price=data['price'])
+            item.save()
+            return JsonResponse({'id': item.id}, status=201)
+        except (ValidationError, json.JSONDecodeError, TypeError, ValueError) as exc:
+            print(exc)
             return JsonResponse({}, status=400)
-        #return JsonResponse(data, status=201)
 
 
 class PostReviewView(View):
     """View для создания отзыва о товаре."""
 
     def post(self, request, item_id):
-    # Здесь должен быть ваш код
-        return JsonResponse(data, status=201)
+
+        auth64 = request.META.get('HTTP_AUTHORIZATION')
+        auth = base64.b64decode(auth64).decode('ascii')
+        username, password = auth.split(':')
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return JsonResponse({}, status=401)
+        if not user.is_staff:
+            return JsonResponse({}, status=403)
+        try:
+            data = json.loads(request.body)
+            if 'grade' in data.keys():
+                data['grade'] = int(data['grade'])
+            validate(data, REVIEW_SCHEMA)
+            if len(Item.objects.filter(id=item_id)):
+                added_item = Item.objects.filter(id=item_id)[0]
+                review = Review(text=data['text'], grade=data['grade'], item=added_item)
+                review.save()
+                return JsonResponse({'id': review.id}, status=201)
+            else:
+                return HttpResponse('No item with such ID', status=404)
+        except (ValidationError, json.JSONDecodeError, TypeError, ValueError):
+            return HttpResponse('Validation error', status=400)
 
 
 class GetItemView(View):
@@ -60,7 +99,35 @@ class GetItemView(View):
     Помимо основной информации выдает последние отзывы о товаре, не более 5
     штук.
     """
-
     def get(self, request, item_id):
-    # Здесь должен быть ваш код
-        return JsonResponse(data, status=200)
+
+        auth64 = request.META.get('HTTP_AUTHORIZATION')
+        auth = base64.b64decode(auth64).decode('ascii')
+        username, password = auth.split(':')
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return JsonResponse({}, status=401)
+        if not user.is_staff:
+            return JsonResponse({}, status=403)
+        if len(Item.objects.filter(id=item_id)):
+            item = Item.objects.filter(id=item_id)[0]
+            reviews_obj = Review.objects.filter(item_id=item_id).order_by('-id')[:5]
+            reviews = []
+            for review in reviews_obj:
+                r = {}
+                r['id'] = review.__dict__['id']
+                r['text'] = review.__dict__['text']
+                r['grade'] = int(review.__dict__['grade'])
+                reviews.append(r)
+
+            data = {
+                'id': int(item_id),
+                'title': item.title,
+                'description': item.description,
+                'price': int(item.price),
+                'reviews': reviews
+            }
+            return JsonResponse(data, status=200)
+        else:
+            return HttpResponse('Item with id {} does not exist'.format(item_id), status=404)
+
