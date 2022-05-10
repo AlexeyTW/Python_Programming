77fd6fc3f9a9319b44530b1392077430f93e6701
Alexey Barinov <barinov_a@inbox.ru>
2022-05-11 00:45:00 +0300

    W6 telebot is in progress

--- playground/course3/w6/w6_telebot/w6_bot.py
+++ playground/course3/w6/w6_telebot/w6_bot.py
@@ -14,7 +14,7 @@ conn = sqlite3.connect('db/places.db', check_same_thread=False)
 cursor = conn.cursor()
 
 commands = ['/add', '/list', '/reset']
-buttons = ['Yes', 'No']
+buttons = ['Да', 'Нет']
 START, SET_NAME, SET_COORDS, SET_PHOTO, ADD_PLACE, USER_COORDS = range(6)
 
 places = defaultdict(lambda: [])
@@ -38,6 +38,8 @@ def update_user_state(message, state):
 def check_coordinates(message):
     text = message.text
     c = text.split(', ')[:2]
+    if len(c) != 2:
+        return False
     try:
         coords = str(set(map(float, c)))[1:-1]
         return coords
@@ -58,7 +60,7 @@ def add(param, value):
 
 def store_place(message):
     keyboard = draw_buttons(buttons)
-    bot.send_message(message.chat.id, 'Do you want to store this place?',
+    bot.send_message(message.chat.id, 'Вы хотите сохранить это место?',
                      reply_markup=keyboard)
 
 
@@ -89,23 +91,23 @@ def calc_dist(origin, destination):
 @bot.callback_query_handler(lambda callback: True)
 def callback_handler(callback):
     names = get_places_names(callback.message)
-    if callback.data == 'Yes':
+    if callback.data == 'Да':
         place_to_db(callback.from_user.id,
                     place['name'],
                     place['coordinates'],
                     place['photo'][-1].file_id if place['photo'] else None)
         bot.send_message(chat_id=callback.from_user.id,
-                         text=f'Place {place["name"]} has been added')
+                         text=f'Место {place["name"]} добавлено')
 
     if callback.data == 'OK':
         cursor.execute('delete from places where user_id = {}'.format(callback.from_user.id))
         conn.commit()
-        bot.send_message(callback.from_user.id, 'Your places have been deleted from the storage')
+        bot.send_message(callback.from_user.id, 'Ваши места были удалены из хранилища данных')
 
     if callback.data in names:
         plc = get_place_by_name(callback.message, callback.data)
-        bot.send_message(callback.from_user.id, f'Place name: {plc[1]},\n'
-                                                f'Coordinates: {plc[2]}')
+        bot.send_message(callback.from_user.id, f'Имя места: {plc[1]},\n'
+                                                f'Координаты: {plc[2]}')
         if plc[3] is not None:
             file = bot.get_file(plc[3])
             img = bot.download_file(file.file_path)
@@ -113,21 +115,21 @@ def callback_handler(callback):
                            photo=img)
         return
 
-    if callback.data == 'Show all places':
+    if callback.data == 'Список 10 недавних мест':
         places_names = get_places_names(callback.message)[-10:]
         keyboard = draw_buttons(places_names)
-        bot.send_message(callback.from_user.id, f'Recent 10 stored places. Select one to get more details',
+        bot.send_message(callback.from_user.id, f'Недавние 10 мест. Выберите, чтобы полуить детали',
                          reply_markup=keyboard)
         return
 
-    if callback.data == 'Show nearest places':
-        bot.send_message(callback.from_user.id, 'Type your coordinates')
+    if callback.data == 'Ближайшее ко мне':
+        bot.send_message(callback.from_user.id, 'Введите ваши координаты')
         update_user_state(callback.message, USER_COORDS)
         return
 
     update_user_state(callback.message, START)
     bot.send_message(chat_id=callback.from_user.id,
-                     text='Waiting for the next command')
+                     text='Жду следующей команды')
 
 @bot.message_handler(func=lambda message: get_user_state(message) == USER_COORDS)
 def get_user_coordinates(message):
@@ -139,35 +141,52 @@ def get_user_coordinates(message):
             dists[calc_dist(origin, i[1])] = i[0]
         nearest_dist = min(dists.keys())
         nearest_place = dists[nearest_dist]
-        bot.send_message(message.chat.id, f'The closest cafe to you is {nearest_place}')
-    update_user_state(message, START)
+        bot.send_message(message.chat.id, f'Ближайшее к вам место: {nearest_place}\n'
+                                          f'Жду следующей команды')
+        update_user_state(message, START)
+    else:
+        bot.send_message(message.chat.id, 'Неверные координаты. Используй формат lat, long')
+        update_user_state(message, USER_COORDS)
 
 
 @bot.message_handler(func=lambda message: get_user_state(message) == START,
                      commands=['add'])
 def command_add(message):
-    bot.send_message(message.chat.id, "Set cafe name")
+    bot.send_message(message.chat.id, "Задай имя места")
     update_user_state(message, SET_NAME)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет! Я помогу тебе сохранить твои любимые места для посещения.\n'
+                                      'Я поддерживаю следующие команды:\n'
+                                      '/add - добавить новое место. Нужно предоставить имя и координаты (можно с фото)\n'
+                                      '/list - Выбор режима отображения мест:\n'
+                                      '- отобразить ближайшее к тебе место\n'
+                                      '- отобразить список 10 недавних мест\n'
+                                      '/reset - удалить все твои места\n\n'
+                                      'Прошу обратить внимание, что ближайшее место определяется путем вычисления\n'
+                                      'кратчайшего расстояния по прямой, потому что сервис GoogleMaps API платный')
 
 
 @bot.message_handler(commands=['list'])
 def list_places(message):
-    list_options = ['Show nearest places', 'Show all places']
+    list_options = ['Ближайшее ко мне', 'Список 10 недавних мест']
     opts_keyboard = draw_buttons(list_options)
-    bot.send_message(message.chat.id, f'Choose the places you want to get',
+    bot.send_message(message.chat.id, f'Какие места вы хотите посмотреть?',
                      reply_markup=opts_keyboard)
 
 
 @bot.message_handler(commands=['reset'])
 def reset_places(message):
     keyboard = draw_buttons(['OK', 'Cancel'])
-    bot.send_message(message.chat.id, 'Do you really want to reset your places?',
+    bot.send_message(message.chat.id, 'Действительно удалить все ваши места?',
                      reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: get_user_state(message) == SET_NAME)
 def set_name(message):
     add('name', message.text)
-    bot.send_message(message.chat.id, "Set coordinates")
+    bot.send_message(message.chat.id, "Задай координаты")
     update_user_state(message, SET_COORDS)
 
 
@@ -175,10 +194,10 @@ def set_name(message):
 def set_coordinates(message):
     coords = check_coordinates(message)
     if not coords:
-        bot.send_message(message.chat.id, 'Incorrect coordinates. Please use format "lat, long"')
+        bot.send_message(message.chat.id, 'Неверные координаты. Используйте формат lat, long')
     else:
         add('coordinates', str(coords))
-        bot.send_message(message.chat.id, "Set photo or type 'skip' to skip adding the photo")
+        bot.send_message(message.chat.id, "Задайте фото или введи 'skip' для пропуска шага")
         update_user_state(message, SET_PHOTO)
 
 
@@ -188,13 +207,13 @@ def set_photo(message):
     add('photo', message.photo)
     update_user_state(message, ADD_PLACE)
     if message.photo is None:
-        bot.send_message(message.chat.id, f"Photo is not set for the place {place['name']}")
+        bot.send_message(message.chat.id, f"Фото не задано для места {place['name']}")
     store_place(message)
 
 
 @bot.message_handler(func=lambda message: message.text not in commands)
 def simple_message(message):
-    bot.send_message(message.chat.id, 'Please type your command')
+    bot.send_message(message.chat.id, 'Введите корректную команду.\n /start для отображения списка команд')
 
 
 
